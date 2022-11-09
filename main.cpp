3ecac52b73aa595ca9a7ad4cab98fd2ec7393f87
nhlee98 <e5slnh9876@gmail.com>
2022-11-09 16:15:16 +0900

    modify stdout

--- main.cpp
+++ main.cpp
@@ -13,13 +13,18 @@ int main() {
         if (infix == "q" || infix == "Q") {
             exit(0); // If user enter the 'q' or 'Q', exit the program.
         }
-        std::cout << "The infix expression : " << infix << std::endl;
+        std::cout << std::endl
+                  << "The infix expression : " << infix << std::endl
+                  << std::endl;
 
         postfix = e.convert_to_postfix(infix);
-        std::cout << "The postfix expression : " << postfix << std::endl;
+        std::cout << "The postfix expression : " << postfix << std::endl
+                  << std::endl;
 
         result = e.evaluate_postfix(postfix);
-        std::cout << "The evaluation result : " << result << std::endl;
+        std::cout << "The evaluation result : " << result << std::endl
+                  << std::endl
+                  << std::endl;
     }
 
     return 0;
